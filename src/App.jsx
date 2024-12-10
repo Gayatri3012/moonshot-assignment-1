559d0d329e692edbb82515fdcce3d51eaa57b129
Gayatri <takawalegayatri@gmail.com>
2024-12-10 21:35:22 +0530

    Added a fetch for glitch server

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Grid from './components/Grid';
 
 function App() {
 
+  //this is a wakeup mechanism for a different RBAC website's glitch server
+  fetch('https://standing-alive-airship.glitch.com/wakeup')
+
   return (
     <>
       <h1>Game of Life</h1>
